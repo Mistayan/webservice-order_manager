c3f68f941f02bd62ca4664d755f3d08c11b2e848
Mistayan <helixmastaz@gmail.com>
2023-05-31 11:23:35 +0200

    objects repositories

---
+++ src/main/java/fr/epsi/rennes/ws/ordermanager/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package fr.epsi.rennes.ws.ordermanager.repository;
+import fr.epsi.rennes.ws.ordermanager.generated.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+}
