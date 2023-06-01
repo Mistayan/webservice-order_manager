7f900467fa021adc71b0f68eb96b7861d5616fda
Mistayan <helixmastaz@gmail.com>
2023-06-01 17:59:34 +0200

    gestion des exceptions de validation data.

--- src/main/java/fr/epsi/rennes/ws/ordermanager/generated/Item.java
+++ src/main/java/fr/epsi/rennes/ws/ordermanager/generated/Item.java
@@ -3,6 +3,7 @@ package fr.epsi.rennes.ws.ordermanager.generated;
 
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,6 +24,7 @@ public class Item implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
+    @Pattern(regexp = "^[a-zA-Z1-9() ]+$", message = "Item name must contain only letters, numbers and spaces ( also accepts () )")
     private int id;
 
     @Column(nullable = false, unique = true)
@@ -30,11 +32,13 @@ public class Item implements Serializable {
 
     // price cannot be negative
     @Column(name = "unit_price", nullable = false, columnDefinition = "float default 0.0")
+    @DecimalMin(value = "0.0", inclusive = true, message = "Price cannot be negative")
     protected float unitPrice;
 
     // sur une commande, représente la quantité commandée à déduire du stock
     // sur un Item, représente la quantité en stock
     @Column(nullable = false, columnDefinition = "int default 0.0")
+    @DecimalMin(value = "0.0", inclusive = true, message = "Quantity cannot be negative")
     private int quantity;
 
     @Override
